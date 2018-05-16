23bc0cf22703958108250c9a6d6071eac305e9bd
Thibault Lacroux <thibault.lacroux@gmail.com>
2018-05-16 13:55:02 +0200

    fix: Rubocop's alerts

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -1,18 +1,18 @@
 Types::QueryType = GraphQL::ObjectType.define do
-  name "Query"
+  name 'Query'
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
 
   field :movie do
     type !Types::MovieType
     argument :id, !types.ID
-    description "Find a Movie by ID"
-    resolve ->(obj, args, ctx) { Movie.find(args["id"]) }
+    description 'Find a Movie by ID'
+    resolve ->(_obj, args, _ctx) { Movie.find(args['id']) }
   end
 
   field :movies do
     type !types[!Types::MovieType]
-    description "Display all movies in db"
-    resolve -> (obj, args, ctx) { Movie.all }
+    description 'Display all movies in db'
+    resolve ->(_obj, _args, _ctx) { Movie.all }
   end
 end
