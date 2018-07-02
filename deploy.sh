649186384063d6161429d75e243cdb2182444877
Thibault Lacroux <thibault.lacroux@gmail.com>
2018-07-02 18:41:25 +0200

    feat(CI): Image build
    * Splitting docker-compose configuration for build & deployment
    * Backend build bundle install with --deployment
    * Using cached deps for build

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git pull
-# Generating JSON GraphQL Schema
-docker run -it --rm --env-file .env -v $PWD/backend:/app -w /app ruby bash -c "bundle install && rails graphql:schema:dump"
-# Transform GraphQL Schema to JS files
-docker run -it --rm -v $PWD:/app -w /app/frontend node:10 bash -c "yarn install && yarn relay"
-
-# Launching services with production docker-compose
-docker-compose -f docker-compose.yml -f docker-compose.production.yml up --build -d
